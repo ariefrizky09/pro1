3de6b3a2c788e0b12282b05e17a096a47247cbef
arief <amrizky13@gmail.com>
2025-11-10 17:05:54 +0700

    belajar fundamental programming

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
+
+    int nyawa = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("ini fungtion Tambah " + Tambah(10, 9, 0));
+        Debug.Log("ini fungtion Poition");
+        Poition();
+        Debug.Log("Nyawa sekarang " + nyawa);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Poition()
+    {
+        Debug.Log("Player use poition");
+        Heal(30);
+    }
+
+    public void Heal(int tambahanNyawa)
+    {
+        nyawa = nyawa + tambahanNyawa;
+    }
+    
+    public int Tambah(int ang1, int ang2, int ang3)
+    {
+        int result = ang1 * ang2 * ang3;
+        return result;
     }
 }
